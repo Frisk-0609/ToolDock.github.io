219da79df212736277fc703d467675f298719552
ToolDock <okirakufufu@gmail.com>
2026-05-08 11:56:14 +0900

    Update todo.js

--- Todolist/todo.js
+++ Todolist/todo.js
@@ -26,4 +26,44 @@ function addTodo() {
   document.getElementById("todoList").appendChild(li);
 
   input.value = "";
+  saveTodos()
 }
+
+function saveTodos() {
+  const list = document.querySelectorAll("#todoList li span");
+  const todos = [];
+
+  list.forEach(item => {
+    todos.push(item.textContent);
+  });
+
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+window.onload = function () {
+  const data = localStorage.getItem("todos");
+
+  if (!data) return;
+
+  const todos = JSON.parse(data);
+
+  todos.forEach(text => {
+    const li = document.createElement("li");
+
+    const span = document.createElement("span");
+    span.textContent = text;
+
+    const btn = document.createElement("button");
+    btn.textContent = "×";
+
+    btn.addEventListener("click", function () {
+      li.remove();
+      saveTodos();
+    });
+
+    li.appendChild(span);
+    li.appendChild(btn);
+
+    document.getElementById("todoList").appendChild(li);
+  });
+};
