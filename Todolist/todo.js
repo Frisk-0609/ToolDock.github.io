8e1fe0ef69335fac808a02f139d22066435aa8f1
ToolDock <okirakufufu@gmail.com>
2026-05-08 11:37:47 +0900

    Update todo.js

--- Todolist/todo.js
+++ Todolist/todo.js
@@ -1,3 +1,5 @@
+let deleteMode = false;
+
 function addTodo() {
   const input = document.getElementById("todoInput");
   const text = input.value;
@@ -7,7 +9,26 @@ function addTodo() {
   const li = document.createElement("li");
   li.textContent = text;
 
+  // 削除モード時はクリックで消す
+  li.addEventListener("click", function () { 
+    if (deleteMode) {
+      li.remove();
+    }
+  });
+
   document.getElementById("todoList").appendChild(li);
 
   input.value = "";
+}
+
+function toggleDeleteMode() {
+  deleteMode = !deleteMode;
+
+  const btn = document.querySelector("button[onclick='toggleDeleteMode()']");
+
+  if (deleteMode) {
+    btn.textContent = "削除モード：ON";
+  } else {
+    btn.textContent = "削除モード：OFF";
+  }
 }
